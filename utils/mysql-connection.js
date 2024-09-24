02c0369590fbe5073a95a72bcffcc152aba78c0d
Alexa <alexarieltonato@hotmail.com>
2024-09-23 20:48:06 -0500

    Se arregla el docker compose

--- utils/mysql-connection.js
+++ utils/mysql-connection.js
@@ -1,12 +1,13 @@
 import mysql from "mysql2/promise";
 
 const config = {
-    host:"mysql-container",
-    user:"root",
-    port:3306,
-    password:"12345",
-    database:"todo_list"
-}
+    host: "mysqldb",
+    user: "root",
+    port: 3306,
+    password: "12345",
+    database: "todo_list"
+};
+
 const connection = await mysql.createConnection(config);
 
 export default connection;
